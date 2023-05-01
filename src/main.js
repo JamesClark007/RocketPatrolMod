61e72d96333e26e641d717447f6801a0db53abc9
JamesClark007 <tahl.clark@gmail.com>
2023-04-30 23:35:33 -0700

    Rocket can now move after being fired

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 // James Clark
 // Rocket Patrol
 // 12 hours
-// Implement a new timing/scoring mechanism that adds time to the clock for successful hits 15
-// Create 4 new explosion sound effects and randomize which one plays on impact 10
-//Track a high score that persists across scenes and display it in the UI (5)
-//
+// Implement a new timing/scoring mechanism that adds time to the clock for successful hits (15)
+// Create 4 new explosion sound effects and randomize which one plays on impact (10)
+// Track a high score that persists across scenes and display it in the UI (5)
+// Allow the player to control the Rocket after it's fired (5)
+
 
 let config = {
     type: Phaser.AUTO,
