343f45eaa35b63afc5d4b0123944a7a330300ac9
JamesClark007 <37253680+JamesClark007@users.noreply.github.com>
2023-05-14 20:22:57 -0700

    Spaceship movement fixed.

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -5,33 +5,26 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         this.points = pointValue;
         this.moveSpeed = game.settings.spaceshipSpeed;
 
-        // Set a constant vertical direction for each spaceship
-        this.verticalDirection = this.getRandomDirection();
+        
     }
 
-    getRandomDirection() {
-        const directions = [-0.2, 0, 0.2];
-        const randomIndex = Math.floor(Math.random() * directions.length);
-        return directions[randomIndex];
-    }
+    
 
     update() {
         // Move horizontally to the left
         this.x -= this.moveSpeed;
 
-        // Move vertically based on the verticalDirection value
-        this.y += this.verticalDirection * this.moveSpeed;
+        
 
         // Wrap around the screen horizontally
         if (this.x <= 0 - this.width) {
             this.x = game.config.width;
-            this.y = Phaser.Math.Between(borderUISize, game.config.height * 5);
+            
         } else if (this.x >= game.config.width + this.width) {
             this.x = 0 - this.width;
         }
 
-        // Clamp the vertical position to stay within the screen
-        this.y = Phaser.Math.Clamp(this.y, this.height / 2, game.config.height - this.height / 2);
+        
     }
 
     reset() {
